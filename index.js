1917d3d42d8582f96f1fbc98d4c1b807eaf8a12f
chad hinds <chadrickj8@gmail.com>
2023-07-10 11:55:51 -0400

    Add Delete, Read and Create

--- index.js
+++ index.js
@@ -31,5 +31,61 @@ app.post('/', async (req, res) => {
     res.send(newBaddie)
 })
 
-// Delete new entry
+
+// Update an entry
+app.put('/:id', async (req, res) => {
+    const baddieID = req.params.id;
+    const newData = req.body;
+
+    try {
+        const updatedBaddie = await baddie.update(newData, {
+            where: { id: baddieID },
+            returning: true
+        })
+        res.send(updatedBaddie)
+    }
+    catch (error) {
+        console.log('Error updating entry:', error);
+    }
+})
+
+// Get specific entry
+app.get('/:id', async (req, res) => {
+    const baddieID = req.params.id
+
+    try {
+        const baddieEntry = baddie.findOne({
+            where: { id: baddieID }
+        })
+        if (baddieEntry === 0) {
+            // Baddie not found
+            return res.status(404).json({ error: 'Entry not found.' });
+        }
+    }
+    catch (error) {
+        console.log('Error finding that entry:', error);
+    }
+})
+
+
+// Delete an entry
+app.delete('/:id', async (req, res) => {
+    const baddieID = req.params.id;
+
+    try {
+        const deletedEntry = await baddie.destroy({
+            where: { id: baddieID }
+        })
+        if (deletedEntry === 0) {
+            // If no rows were deleted, the ID doesn't exist
+            return res.status(404).json({ error: 'Entry not found.' });
+        }
+        // Entry was deleted
+        res.send('Entry deleted successfully!')
+    }
+    catch (error) {
+        console.log('Error updating entry:', error);
+    }
+})
+
 app.listen(PORT, console.log(`Server started on port ${PORT}!`));
