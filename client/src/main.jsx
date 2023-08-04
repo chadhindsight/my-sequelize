40c19a8284b85f7d801ea20f3ad9567afefbe59e
chad hinds <chadrickj8@gmail.com>
2023-08-04 11:48:23 -0400

    Move error page to routes folder

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,13 +6,14 @@ import {
 } from "react-router-dom";
 import "./index.css";
 import Root from "./routes/root";
-import ErrorPage from "./error-page";
+import ErrorPage from "./routes/error-page";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    // The '/addbaddie' route should be nested
   },
 ]);
 
