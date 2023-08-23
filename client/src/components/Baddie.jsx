ba9118d5621732b3419486e4b412c4db16fb43a3
chad hinds <chadrickj8@gmail.com>
2023-08-23 13:30:55 -0400

    Comment out broken image link that will be replaced

--- client/src/components/Baddie.jsx
+++ client/src/components/Baddie.jsx
@@ -13,9 +13,9 @@ export default function Baddie() {
     return (
     <>
 <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <a href="#">
+    {/* <a href="#">
         <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
-    </a>
+    </a> */}
     <div className="p-5">
         <a href="#">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
