38d02e3e9f93fbcfd1a856a77dfbccee97e138d9
chad hinds <chadrickj8@gmail.com>
2023-08-23 12:42:11 -0400

    Add comment explaining newly added configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Added to allow the client side to access relative paths 'api/baddies'
   server: {
     port: 5173,
     proxy: {
