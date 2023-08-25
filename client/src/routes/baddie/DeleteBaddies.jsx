0f8563dd5970621411733e91cdfd38d42a336257
chad hinds <chadrickj8@gmail.com>
2023-08-25 13:41:33 -0400

    Add update and delete Baddie action

--- client/src/routes/baddie/DeleteBaddies.jsx
+++ client/src/routes/baddie/DeleteBaddies.jsx
@@ -1,12 +1,7 @@
-
-const DeleteBaddies = () => {
-        // on '/api/baddies/:id'
-
-    return (
-        <div>
-            
-        </div>
-    );
-};
-
-export default DeleteBaddies;
+export async function action({ params }) {
+  const response = await fetch(`/api/baddies/${params.noteId}`, {
+    method: "DELETE",
+  });
+  console.log(response)
+  return null;
+}
--- client/src/routes/baddie/UpdateBaddie.jsx
+++ client/src/routes/baddie/UpdateBaddie.jsx
@@ -1,11 +1,15 @@
-
-const UpdateBaddie = () => {
-     // on '/api/baddies/:id'
-    return (
-        <div>
-            
-        </div>
-    );
-};
-
-export default UpdateBaddie;
+export async function action({ params, request }) {
+  const formData = await request.formData();
+  const response = await fetch(`/api/baddies/${params.baddieId}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(Object.fromEntries(formData)),
+  });
+  if (!response.ok) {
+    const { errors } = await response.json();
+    return errors;
+  }
+  return null;
+}
