23284fca00b1f0fe7c1b6b95f30fdd1f786f12e3
chad hinds <chadrickj8@gmail.com>
2023-08-28 14:04:37 -0400

    Ensure the list and baddie card are displayed next to each other horizontally

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer
+        <footer 
             className="bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left footer">
             <div className="p-4 text-center text-neutral-700 dark:text-neutral-200">
                 © {new Date().getFullYear()} Copyright:
