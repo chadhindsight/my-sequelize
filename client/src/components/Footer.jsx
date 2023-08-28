a68c8f8b867fbbe2833b57289633f63c0700a698
chad hinds <chadrickj8@gmail.com>
2023-08-28 14:51:09 -0400

    Make footer text slight lighter

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer 
-            className="bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left footer">
+            className="bg-neutral-200 text-center dark:bg-neutral-500 lg:text-left footer">
             <div className="p-4 text-center text-neutral-700 dark:text-neutral-200">
                 © {new Date().getFullYear()} Copyright:
                 <a
