cd5680d37fe6a6a213f8680588702fb497d358c8
chad hinds <chadrickj8@gmail.com>
2023-08-30 11:32:29 -0400

    For each baddie, Render a corresponding link in the nav section

--- client/src/routes/root.jsx
+++ client/src/routes/root.jsx
@@ -1,12 +1,34 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
-import BaddieList from "../components/BaddieList";
-export default function Root() {
+import { useLoaderData } from "react-router-dom";
+
+export async function loader() {
+    let url = "/api/baddies";
+    const response = await fetch(url);
+    const baddies = await response.json();
+    return { baddies };
+  }
+  
+  export default function Root() {
+    let baddieTextDm = "text-md underline dark:text-white";
+    const {baddies} = useLoaderData()
+    console.log('trash',baddies)
+
   return (
     <main className="bg-neutral-200 text-center dark:bg-neutral-700">
     <section id="baddie-parent">
-    <BaddieList/>
-     
+       <aside id="sidebar">
+            <h1 className="text-3xl font-bold dark:text-white">Baddies</h1>
+            <nav>
+                <ul>
+                    {baddies.map(baddie =>(
+                      <li  className={baddieTextDm} key={baddie.id}>
+                        <Link to={`/baddies/${baddie.id}`}>{baddie.name}</Link>
+                      </li>
+                    ))}
+                </ul>
+            </nav>
+        </aside>      
        <section id="detail">
         <Outlet />
       </section>
