8cb400168d2f319b2276e5b4f6b762afea98728e
chad hinds <chadrickj8@gmail.com>
2023-09-04 13:38:51 -0400

    first iteration of setting up the update model

--- client/src/routes/baddie/Baddie.jsx
+++ client/src/routes/baddie/Baddie.jsx
@@ -1,4 +1,5 @@
-import { useLoaderData, useFetcher, redirect } from "react-router-dom";
+import { useState } from "react";
+import { useLoaderData, useFetcher, useNavigate } from "react-router-dom";
 
 export async function loader({params}) {     
     let url = `/api/baddies/${params.baddieId}`;
@@ -9,16 +10,40 @@ export async function loader({params}) {
 }
 
 export default function Baddie() {
-    const fetcher = useFetcher();
+   const fetcher = useFetcher();
    const {baddie} = useLoaderData();
-   console.log(baddie)
+   const navigate = useNavigate();
+   const [toggleModal, setToggleModal] = useState(false);
 
-    const onDelete = (event) => {
-              if (!confirm("Please confirm you want to delete this record.")) {
-                event.preventDefault();
-                return redirect("/");
-            }
-    }   
+    const onSubmitDelete = async (event) => {
+  if (!window.confirm("Please confirm you want to delete this record.")) {
+    event.preventDefault();
+    return;
+  }
+
+  try {
+    await fetch(`/api/baddies/${baddie.id}`, { method: "DELETE" });
+    navigate("/"); // Navigate back to the home route after successful deletion
+  } catch (error) {
+    console.error("Error deleting entry:", error);
+    // Handle errors here if necessary
+  }
+    };
+
+    
+    // NB: Update Baddie client side route is /update/${baddie.id}
+    const handleUpdate = () => {
+    // Toggle the modal by setting the opposite value of toggleModal
+    setToggleModal(!toggleModal);
+  };
+
+    // const onUpdateSubmit = async (event) => {
+        
+    // }
+  
+
+ 
+
 
 return (
 
@@ -28,19 +53,21 @@ return (
         <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{baddie.name}</h5>
         <span className="text-sm text-gray-500 dark:text-gray-400">Age: {baddie.age}</span>
         <section className="flex mt-4 space-x-3 md:mt-6">
-            <button href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Update</button>
+            <button  className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            onClick={handleUpdate}>
+                Update
+            </button>
              <fetcher.Form
             method="delete"
-            action={`/baddies/${baddie.id}/destroy`}
-            onSubmit={onDelete}
+            action={`/baddies/${baddie.id}`}
+            onSubmit={onSubmitDelete}
           >
-
-            <button href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Delete</button>
+            {/* This button just deals with rendering the update modal */}
+            <button className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Delete</button>
           </fetcher.Form>
         </section>
     </section>
 </section>
-
    )
 }
 
