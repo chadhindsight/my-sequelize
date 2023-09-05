6c1d42121db9a0d4804cd8b13a15d7308bad7bdb
chad hinds <chadrickj8@gmail.com>
2023-09-05 14:44:18 -0400

    Remove superfluous logs

--- client/src/routes/baddie/Baddie.jsx
+++ client/src/routes/baddie/Baddie.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { useLoaderData, useFetcher, useNavigate } from "react-router-dom";
+import { useLoaderData, useFetcher, useNavigate, Link } from "react-router-dom";
 
 export async function loader({params}) {     
     let url = `/api/baddies/${params.baddieId}`;
@@ -13,7 +12,6 @@ export default function Baddie() {
    const fetcher = useFetcher();
    const {baddie} = useLoaderData();
    const navigate = useNavigate();
-   const [toggleModal, setToggleModal] = useState(false);
 
     const onSubmitDelete = async (event) => {
   if (!window.confirm("Please confirm you want to delete this record.")) {
@@ -30,21 +28,6 @@ export default function Baddie() {
   }
     };
 
-    
-    // NB: Update Baddie client side route is /update/${baddie.id}
-    const handleUpdate = () => {
-    // Toggle the modal by setting the opposite value of toggleModal
-    setToggleModal(!toggleModal);
-  };
-
-    // const onUpdateSubmit = async (event) => {
-        
-    // }
-  
-
- 
-
-
 return (
 
 <section className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">    
@@ -53,16 +36,15 @@ return (
         <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{baddie.name}</h5>
         <span className="text-sm text-gray-500 dark:text-gray-400">Age: {baddie.age}</span>
         <section className="flex mt-4 space-x-3 md:mt-6">
-            <button  className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            onClick={handleUpdate}>
+            <Link  className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            to={`/baddies/${baddie.id}/update`}>
                 Update
-            </button>
+            </Link>
              <fetcher.Form
             method="delete"
             action={`/baddies/${baddie.id}`}
             onSubmit={onSubmitDelete}
           >
-            {/* This button just deals with rendering the update modal */}
             <button className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Delete</button>
           </fetcher.Form>
         </section>
