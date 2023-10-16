283af53b9a5526b4dae19cb9a82097a9f70de22d
chad hinds <chadrickj8@gmail.com>
2023-10-16 12:02:30 -0400

    explain /api/baddies route

--- client/src/routes/baddie/AddBaddie.jsx
+++ client/src/routes/baddie/AddBaddie.jsx
@@ -24,7 +24,7 @@ export async function action({ request }) {
 } 
 
 const AddBaddie = () => {
-    // on client side the endpoint is '/api/baddies/new', backend its '/api/baddies'
+    // on client side the endpoint is '/api/baddies/new' and the  backend its '/api/baddies'
     const errors = useActionData()
     return (
        
