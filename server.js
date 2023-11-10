9862f79cb9559ecaf3a35f54b0b4ba93f6db9f9f
chad hinds <chadrickj8@gmail.com>
2023-11-10 10:43:17 -0500

    Adjust comment regarding how json gets parsed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const PORT = 8000;
 const app = express();
 
-// Allows you to parse the body
+// This allows you to parse the body
 app.use(express.json());
 
 //Database
