3e576780db938562b83593d67035cf0b9954d6e4
chad hinds <chadrickj8@gmail.com>
2023-11-10 10:36:09 -0500

    Change card title to an h4

--- client/src/routes/baddie/Baddies.jsx
+++ client/src/routes/baddie/Baddies.jsx
@@ -20,9 +20,9 @@ const Baddies = () => {
             to={`/baddies/${baddie.id}`}
             className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
           >
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            <h4 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
               {baddie.name}
-            </h5>
+            </h4>
             <p className="font-normal text-gray-700 dark:text-gray-400">
               Here are the biggest enterprise technology acquisitions of 2024 so
               far, in reverse chronological order.
