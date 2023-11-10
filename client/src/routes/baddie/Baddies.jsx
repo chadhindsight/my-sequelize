e976753514f0a65ed624f25c34d5e0e20c19d631
chad hinds <chadrickj8@gmail.com>
2023-11-10 10:41:00 -0500

    Rearrange where the age data gets displayed so it's easier to read

--- client/src/routes/baddie/Baddies.jsx
+++ client/src/routes/baddie/Baddies.jsx
@@ -23,11 +23,11 @@ const Baddies = () => {
             <h4 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
               {baddie.name}
             </h4>
+            <h3 className="dark:text-white">{baddie.age}</h3>
             <p className="font-normal text-gray-700 dark:text-gray-400">
               Here are the biggest enterprise technology acquisitions of 2024 so
               far, in reverse chronological order.
             </p>
-            <p>{baddie.age}</p>
           </Link>
         );
       })}
