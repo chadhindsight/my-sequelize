083ad3a55a203ee7698f38fc6ea8e052b3cb9691
chad hinds <chadrickj8@gmail.com>
2023-11-17 23:18:23 -0500

    change CTA displayed for updating an entry

--- client/src/routes/baddie/Baddie.jsx
+++ client/src/routes/baddie/Baddie.jsx
@@ -37,7 +37,7 @@ return (
         <section className="flex mt-4 space-x-3 md:mt-6">
             <Link  className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             to={`/baddies/${baddie.id}/update`}>
-                Update Baddie
+                Update A Baddie
             </Link>
              <fetcher.Form
             method="delete"
