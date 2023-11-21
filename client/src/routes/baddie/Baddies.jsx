ba198d964ccf922cf375c010869f0636ceb6ab4b
chad hinds <chadrickj8@gmail.com>
2023-11-21 15:28:47 -0500

    Add todo note for tomorrow

--- client/src/routes/baddie/Baddies.jsx
+++ client/src/routes/baddie/Baddies.jsx
@@ -29,6 +29,7 @@ const Baddies = () => {
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
               {baddie.name}
             </h5>
+            {/* TODO: change this text to some custom content */}
             <p className="font-normal text-gray-700 dark:text-gray-400">
               Here are the biggest enterprise technology acquisitions of 2024 so
               far, in reverse chronological order.
