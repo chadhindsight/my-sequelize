0898e97f2da89df1f119c29290136858469b2df0
chad hinds <chadrickj8@gmail.com>
2023-11-22 11:43:10 -0500

    Remove TODO comment

--- client/src/routes/baddie/Baddies.jsx
+++ client/src/routes/baddie/Baddies.jsx
@@ -30,7 +30,6 @@ const Baddies = () => {
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
               {baddie.name}
             </h5>
-            {/* TODO: change this text to some custom content */}
             <p className="font-normal text-gray-700 dark:text-gray-400">
               {randomizeBaddieInfo()}
             </p>
