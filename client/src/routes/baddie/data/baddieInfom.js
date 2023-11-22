61bf3d5d5fedc074e4bcb97a2056f471d1d249b3
chad hinds <chadrickj8@gmail.com>
2023-11-22 11:42:12 -0500

    Correct import mistake

--- client/src/routes/baddie/data/baddieInfom.js
+++
@@ -1,8 +0,0 @@
-const information = [
-
-]
-
-export const randomizeBaddieInfo = () => {
-    const entryToDisplay = Math.floor(Math.random() * information.length);
-    return information[entryToDisplay];
-};
