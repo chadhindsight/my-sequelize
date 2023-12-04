51aa72851392ca0beb3003b60af58d1846a4069e
chad hinds <chadrickj8@gmail.com>
2023-12-04 11:27:35 -0500

    Remove unsued import

--- client/Footer.test.jsx
+++ client/Footer.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
 import Footer from "./src/components/Footer";
 
