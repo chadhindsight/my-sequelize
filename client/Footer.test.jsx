bc82249837d30d4e4e439572656e215a12a261b0
chad hinds <chadrickj8@gmail.com>
2023-12-04 10:54:32 -0500

    Remove comment

--- client/Footer.test.jsx
+++ client/Footer.test.jsx
@@ -1,20 +1,22 @@
-// Tests for the footer content
-import { render, screen } from '@testing-library/react';
-import { describe, it, expect } from 'vitest';
-import Footer from './src/components/Footer';
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import Footer from "./src/components/Footer";
 
-describe('Footer Component', () => {
-  it('renders a link', () => {
+describe("Footer Component", () => {
+  it("renders a link", () => {
     render(<Footer />);
 
     // Find the link element by its role
-    const linkElement = screen.getByRole('link');
+    const linkElement = screen.getByRole("link");
 
     // Footer Assertions
     expect(linkElement).toBeInTheDocument();
-    expect(linkElement).toHaveAttribute('href', 'https://chadhindsight.github.io');
-    expect(linkElement).toHaveAttribute('target', '_blank');
-    expect(linkElement).toHaveAttribute('rel', 'noreferrer');
-    expect(linkElement.textContent).toBe('Chad Hinds');
+    expect(linkElement).toHaveAttribute(
+      "href",
+      "https://chadhindsight.github.io"
+    );
+    expect(linkElement).toHaveAttribute("target", "_blank");
+    expect(linkElement).toHaveAttribute("rel", "noreferrer");
+    expect(linkElement.textContent).toBe("Chad Hinds");
   });
 });
