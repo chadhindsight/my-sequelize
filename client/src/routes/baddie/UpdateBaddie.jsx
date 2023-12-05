0413e0e895fa522e3427686d28c429083cf29204
chad hinds <chadrickj8@gmail.com>
2023-12-05 12:53:09 -0500

    Ensure button styling for the update button is consistent with the other buttons used in the app

--- client/src/routes/baddie/UpdateBaddie.jsx
+++ client/src/routes/baddie/UpdateBaddie.jsx
@@ -1,14 +1,15 @@
 import { Form, redirect, useActionData } from "react-router-dom";
 
-
 export async function action({ params, request }) {
   let formData = await request.formData();
   let baddieData = Object.fromEntries(formData);
-  
+
   try {
     // Filter out empty fields from baddieData
-    let nonEmptyFields = Object.entries(baddieData).filter(([, value]) => value !== "")
-    if(baddieData.age) baddieData.age = parseInt(baddieData.age);
+    let nonEmptyFields = Object.entries(baddieData).filter(
+      ([, value]) => value !== ""
+    );
+    if (baddieData.age) baddieData.age = parseInt(baddieData.age);
 
     const response = await fetch(`/api/baddies/${params.baddieId}`, {
       method: "PATCH",
@@ -17,7 +18,7 @@ export async function action({ params, request }) {
       },
       body: JSON.stringify(Object.fromEntries(nonEmptyFields)),
     });
-     if (response.ok) {
+    if (response.ok) {
       return redirect(`/baddies/${params.baddieId}`);
     }
     if (!response.ok) {
@@ -32,30 +33,50 @@ export async function action({ params, request }) {
   }
 }
 
-
 const UpdateBaddie = () => {
-    const errors = useActionData()
-    return (
-       
-<div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-    {errors && <div className="text-red-300">{errors}</div>}
-    <Form method="patch" className="space-y-6" >
-        <h5 className="text-xl font-medium text-gray-900 dark:text-white">Update {}</h5>
+  const errors = useActionData();
+  return (
+    <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+      {errors && <div className="text-red-300">{errors}</div>}
+      <Form method="patch" className="space-y-6">
+        <h5 className="text-xl font-medium text-gray-900 dark:text-white">
+          Update {}
+        </h5>
         <div>
-            <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-            <input type="text" name="name" id="Updatename" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Baddie Name" />
+          <label
+            htmlFor="name"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="Updatename"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+            placeholder="Baddie Name"
+          />
         </div>
         <div>
-            <label htmlFor="age" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Age</label>
-            <input name="age" id="Updatepassword" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Baddie Age"/>
+          <label
+            htmlFor="age"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Age
+          </label>
+          <input
+            name="age"
+            id="Updatepassword"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+            placeholder="Baddie Age"
+          />
         </div>
         <input
-        className="bg-blue-500 hover:bg-blue-600 text-white transition mt-4 py-2 cursor-pointer "
-        type="submit"
-      ></input>
-    </Form>
-</div>
-
-    );
+          className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
+          type="submit"
+        ></input>
+      </Form>
+    </div>
+  );
 };
 export default UpdateBaddie;
