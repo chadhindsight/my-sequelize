2ac802066c559de13d8edac04daa684c082815fc
chad hinds <chadrickj8@gmail.com>
2023-12-06 11:22:49 -0500

    add baddie-header as data test id

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,5 +1,12 @@
 const Header = () => {
-  return <h1 className="text-3xl font-bold dark:text-white">Baddies</h1>;
+  return (
+    <h1
+      className="text-3xl font-bold dark:text-white"
+      data-testid="baddie-header"
+    >
+      Baddies
+    </h1>
+  );
 };
 
 export default Header;
