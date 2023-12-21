b0c5a955c47b6c26c6f7b68a8af57a5cf00b2bdf
chad hinds <chadrickj8@gmail.com>
2023-12-21 16:16:34 -0500

    Change heading

--- client/src/routes/baddie/Baddies.jsx
+++ client/src/routes/baddie/Baddies.jsx
@@ -25,9 +25,9 @@ const Baddies = () => {
               alt="Bonnie profile"
               src={`/images/${randomizeBaddiePhoto()}`}
             />
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            <h4 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
               {baddie.name}
-            </h5>
+            </h4>
             <p className="font-normal text-gray-700 dark:text-gray-400">
               {randomizeBaddieInfo()}
             </p>
