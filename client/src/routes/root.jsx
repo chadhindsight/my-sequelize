b2f5c688a404318bbe66524d884668393d8012a9
chad hinds <chadrickj8@gmail.com>
2023-12-21 16:07:48 -0500

    Change button text

--- client/src/routes/root.jsx
+++ client/src/routes/root.jsx
@@ -34,7 +34,7 @@ export default function Root() {
           </nav>
           <Link to="/baddies/new">
             <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
-              New Entry
+              Add New Entry
             </button>
           </Link>
         </aside>
